091a2f2fa397facbfdcfe05fed3327c32df2504b
Bodik <cvantymgeymshhhhh@gmail.com>
2026-07-01 14:52:06 +0300

    Added entities to dev branch (#29)

---
+++ back/KTC.DAL/Repositories/Notification/INotificationRepository.cs
@@ -0,0 +1,17 @@
+﻿using KTC.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KTC.DAL.Repositories.Notification
+{
+    public interface INotificationRepository
+        : IGenericRepository<NotificationEntity>
+    {
+        Task<List<NotificationEntity>> GetUserNotificationsAsync(string userId);
+
+        Task MarkAsReadAsync(string id);
+    }
+}
